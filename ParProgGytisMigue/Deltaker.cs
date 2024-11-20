0c56d315665ebbc2d7ec5b1fb6c38ba443f00ee1
OMFrydenlund <migfrydenlund@gmail.com>
2024-11-20 13:58:37 +0100

    Ferdig med par prog

---
+++ ParProgGytisMigue/Deltaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParProgGytisMigue
+{
+    internal class Deltaker
+    {
+        public string Navn { get; set; }
+        public int Age { get; set; }
+        public string Hobby { get; set; }
+
+        public void PresenterDeltaker()
+        {
+            Console.WriteLine($"Hei mitt navn er {Navn}. Jeg er {Age} år gammel, og jeg er en {Hobby} på fritiden.");
+        }
+
+    }
+}
--- ParProgGytisMigue/Program.cs
+++ ParProgGytisMigue/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            
+            int hvorMangeFolk = 0;
+
+            Deltaker gytis = new Deltaker();
+            gytis.Navn = "Gytis";
+            gytis.Age = 24;
+            gytis.Hobby = "Bokser";
+
+            Deltaker miguel = new Deltaker();
+            miguel.Navn = "Miguel";
+            miguel.Age = 105;
+            miguel.Hobby = "Fisker";
+
+            gytis.PresenterDeltaker();
+            miguel.PresenterDeltaker();
+
+            List<Deltaker> participants = new List<Deltaker>();
+            participants.Add(miguel);
+            participants.Add(gytis);
+
+            foreach (Deltaker d in participants)
+            {
+                hvorMangeFolk++;
+            }
+            Console.ReadLine();
+          
         }
     }
 }
